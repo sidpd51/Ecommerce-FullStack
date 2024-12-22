aba3438d87a52b9335e203241bd9dfe186f1527b
sidpd51 <sidpd510@gmail.com>
2024-12-22 22:06:22 +0530

    feat: add search page

--- ecommerce-frontend/src/main.tsx
+++ ecommerce-frontend/src/main.tsx
@@ -1,6 +1,5 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './styles/app.scss'
 
 createRoot(document.getElementById('root')!).render(
--- ecommerce-frontend/src/pages/Search.tsx
+++ ecommerce-frontend/src/pages/Search.tsx
@@ -1,7 +1,76 @@
+import { useState } from "react"
+import ProductCard from "../components/ProductCard";
 
 function Search() {
+
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("");
+  const [maxPrice, setMaxPrice] = useState(100000);
+  const [category, setCategory] = useState(100000);
+  const [page, setPage] = useState(1);
+  const addToCartHandler = () => { }
+  const isNextPage = page < 4;
+  const isPrevPage = page > 1;
+
   return (
-    <div>Search</div>
+    <div className="product-search-page">
+      <aside>
+        <h2>Filters</h2>
+        <div>
+          <h4>Sort</h4>
+          <select
+            value={sort}
+            onChange={(e) => setSort(e.target.value)}
+          >
+            <option value="">None</option>
+            <option value="asc">Price (Low to High)</option>
+            <option value="des">Price (High to Low)</option>
+          </select>
+        </div>
+        <div>
+          <h4>Max Price: {maxPrice || ""}</h4>
+          <input
+            type="range"
+            min={100}
+            max={100000}
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(Number(e.target.value))} />
+        </div>
+        <div>
+          <h4>Category</h4>
+          <select
+            value={category}
+            onChange={(e) => setCategory(Number(e.target.value))}
+          >
+            <option value="">All</option>
+            <option value="sample1">Sample 1</option>
+            <option value="sample2">Sample 2</option>
+          </select>
+        </div>
+      </aside>
+      <main>
+        <h1>Products</h1>
+        <input
+          type="text"
+          placeholder="search by name..."
+          value={search}
+          onChange={e => setSearch(e.target.value)} />
+        <div className="search-product-list">
+          <ProductCard
+            productId="123"
+            name="macbook"
+            price={90000}
+            photo="https://m.media-amazon.com/images/I/61CkOvTefvL._SL1500_.jpg"
+            handler={addToCartHandler}
+            stock={123} />
+        </div>
+        <article>
+          <button disabled={!isPrevPage} onClick={() => setPage(prev => prev - 1)}>Prev</button>
+          <span>{page} of {4}</span>
+          <button disabled={!isNextPage} onClick={() => setPage(prev => prev + 1)}>Next</button>
+        </article>
+      </main>
+    </div>
   )
 }
 
